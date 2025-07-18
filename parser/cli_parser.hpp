9eb1290af77aea02cdef031c589530383d70579c
JonasCarlstroem <82809067+JonasCarlstroem@users.noreply.github.com>
2025-07-18 17:43:21 +0200

    some changes

--- parser/cli_parser.hpp
+++ parser/cli_parser.hpp
@@ -27,47 +27,56 @@ class parser {
     std::unordered_map<string, option> options_;
     std::vector<argument> global_args_;
     logging::logger* logger_;
+    std::unique_ptr<parse_result> last_result;
 
-    const argument* find_argument_definition(const string& option, const string& name) const {
+    const argument* find_argument_definition(
+        const string& option,
+        const string& name
+    ) const {
         if (!option.empty()) {
             auto it = options_.find(option);
             if (it != options_.end()) {
                 for (const auto& arg : it->second.args()) {
-                    if (arg.matches(name))
-                        return &arg;
+                    if (arg.matches(name)) return &arg;
                 }
             }
         }
 
         for (const auto& arg : global_args_) {
-            if (arg.matches(name))
-                return &arg;
+            if (arg.matches(name)) return &arg;
         }
 
         return nullptr;
     }
 
   public:
-    parser(logging::logger* logger) : logger_(logger) {}
+    parser(logging::logger* logger)
+        : logger_(logger) {}
 
     void add_option(option opt) { options_[opt.name()] = std::move(opt); }
 
     void add_global_argument(argument arg) { global_args_.emplace_back(std::move(arg)); }
 
-    parse_result parse(int argc, char** argv) {
-        parse_result result;
+    const parse_result& get_last_result() { return *last_result; }
+
+    const parse_result& parse(
+        int argc,
+        char** argv
+    ) {
+        if (last_result) last_result.reset();
+        last_result       = std::make_unique<parse_result>();
 
         bool found_option = false;
         string current_option;
 
         for (const auto& arg : global_args_) {
-            result.add_argument(arg.clone_with_current_state());
+            last_result->add_argument(arg.clone_with_current_state());
         }
 
         auto copy_option_args = [&](const string& opt_name) {
             const auto& opt = options_.at(opt_name);
             for (const auto& arg : opt.args()) {
-                result.add_argument(arg.clone_with_current_state());
+                last_result->add_argument(arg.clone_with_current_state());
             }
         };
 
@@ -81,7 +90,7 @@ class parser {
                     throw std::runtime_error("Invalid command: " + token);
                 }
 
-                result.set_chosen_option(token);
+                last_result->set_chosen_option(token);
                 current_option = token;
                 copy_option_args(token);
                 found_option = true;
@@ -116,7 +125,7 @@ class parser {
                     }
                 }
 
-                argument* existing = result.find_argument(name);
+                argument* existing = last_result->find_argument(name);
                 if (!existing)
                     throw std::runtime_error("Invalid argument: " + token);
 
@@ -129,7 +138,7 @@ class parser {
             ++i;
         }
 
-        return result;
+        return *last_result;
     }
 };
 
